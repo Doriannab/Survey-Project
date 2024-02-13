ad9019e1d2656275bd17ffb7794aa98c3a67d578
reymouhid95 <thiernooury89@gmail.com>
2024-02-13 13:11:08 +0000

    redirection

--- src/pages/authentification/Inscription.jsx
+++ src/pages/authentification/Inscription.jsx
@@ -4,9 +4,11 @@ import { useDispatch } from "react-redux";
 import { registerUser } from "../../components/services/AuthServices";
 import { setUser, setToken } from "../../components/features/AuthSlice";
 import { Toaster, toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 const Inscription = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -43,6 +45,7 @@ const Inscription = () => {
         password2: "",
         tc: false,
       });
+      navigate("/connexion");
     } catch (error) {
       console.error("Erreur lors de l'inscription :", error);
       toast.error(
@@ -53,7 +56,7 @@ const Inscription = () => {
 
   return (
     <div className="flex items-center justify-center h-screen ">
-      <Toaster />
+      <Toaster position="top-left" />
       <div className="max-w-md mx-auto mt-6 p-6 bg-white shadow-lg rounded-md backdrop-filter backdrop-blur-sm">
         <h2 className="text-gray-800 text-3xl mb-6 font-bold text-center">
           Pulso
