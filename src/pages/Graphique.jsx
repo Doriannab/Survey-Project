e8fd98d654f1f87629c6a7a7a8e0a59826659f18
reymouhid95 <thiernooury89@gmail.com>
2024-02-19 16:43:18 +0000

    graphique deleted

--- src/pages/Graphique.jsx
+++ src/pages/Graphique.jsx
@@ -1,34 +1,34 @@
-/* eslint-disable no-unused-vars */
-import { Component } from "react";
-import CanvasJSReact from "@canvasjs/react-charts";
+// /* eslint-disable no-unused-vars */
+// import { Component } from "react";
+// import CanvasJSReact from "@canvasjs/react-charts";
 
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-class Graphique extends Component {
-  render() {
-    const options = {
-      title: {
-        text: "Basic Column Chart",
-      },
-      data: [
-        {
-          type: "column",
-          dataPoints: [
-            { label: "Laravel", y: 10 },
-            { label: "React", y: 15 },
-            { label: "Django", y: 25 },
-            { label: "PHP", y: 30 },
-            { label: "Python", y: 28 },
-          ],
-        },
-      ],
-    };
-    return (
-      <div className="w-full text-center">
-        <CanvasJSChart options={options} />
-      </div>
-    );
-  }
-}
+// var CanvasJS = CanvasJSReact.CanvasJS;
+// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+// class Graphique extends Component {
+//   render() {
+//     const options = {
+//       title: {
+//         text: "Basic Column Chart",
+//       },
+//       data: [
+//         {
+//           type: "column",
+//           dataPoints: [
+//             { label: "Laravel", y: 10 },
+//             { label: "React", y: 15 },
+//             { label: "Django", y: 25 },
+//             { label: "PHP", y: 30 },
+//             { label: "Python", y: 28 },
+//           ],
+//         },
+//       ],
+//     };
+//     return (
+//       <div className="w-full text-center">
+//         <CanvasJSChart options={options} />
+//       </div>
+//     );
+//   }
+// }
 
-export default Graphique;
+// export default Graphique;
